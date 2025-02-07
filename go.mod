de4fb71a6cdfbb23ed8a1acfbba3eb26c888527b
Adam Bender <abender@gmail.com>
2025-02-06 19:55:57 -0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bitlux/vpnapi
+
+go 1.23.4
---
+++ vpnapi.go
@@ -0,0 +1,115 @@
+package vpnapi
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+const baseURL = "https://vpnapi.io/api/"
+
+var ErrRateLimited = errors.New("rate limited, try again later")
+
+type Security struct {
+	VPN   bool `json:"vpn"`
+	Proxy bool `json:"proxy"`
+	Tor   bool `json:"tor"`
+	Relay bool `json:"relay"`
+}
+
+type Location struct {
+	City              string `json:"city"`
+	Region            string `json:"region"`
+	Country           string `json:"country"`
+	Continent         string `json:"contient"`
+	RegionCode        string `json:"region_code"`
+	ContinentCode     string `json:"continent_code"`
+	Latitude          string `json:"latitude"`
+	Longitude         string `json:"longitude"`
+	TimeZone          string `json:"time_zone"`
+	LocaleCode        string `json:"locale_code"`
+	MetroCode         string `json:"metro_code"`
+	IsInEuropeanUnion bool   `json:"is_in_european_union"`
+}
+
+type Network struct {
+	Network                      string `json:"network"`
+	AutonomousSystemNumber       string `json:"autonomous_system_number"`
+	AutonomousSystemOrganization string `json:"autonomous_system_organization"`
+}
+
+type Response struct {
+	IP       string   `json:"ip"`
+	Security Security `json:"security"`
+	Location Location `json:"location"`
+	Network  Network  `json:"network"`
+}
+
+type Client struct {
+	apiKey string
+}
+
+func New(apiKey string) *Client {
+	return &Client{apiKey: apiKey}
+}
+
+func (c *Client) Query(ip string) (*Response, error) {
+	resp, err := http.Get(baseURL + ip + "?" + c.apiKey)
+	if err != nil {
+		return nil, err
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	if code := resp.StatusCode; code > 299 {
+		if code == 429 {
+			return nil, ErrRateLimited
+		}
+		return nil, fmt.Errorf("status code %d", code)
+	}
+
+	ret := &Response{}
+	if err = json.Unmarshal(body, ret); err != nil {
+		return nil, err
+	}
+
+	return ret, nil
+}
+
+/*
+	{
+    "ip": "8.8.8.8",
+    "security": {
+        "vpn": false,
+        "proxy": false,
+        "tor": false,
+        "relay": false,
+    },
+    "location": {
+        "city": "",
+        "region": "",
+        "country": "United States",
+        "continent": "North America",
+        "region_code": "",
+        "country_code": "US",
+        "continent_code": "NA",
+        "latitude": "37.7510",
+        "longitude": "-97.8220",
+        "time_zone": "America/Chicago",
+        "locale_code": "en",
+        "metro_code": "",
+        "is_in_european_union": false
+    },
+    "network": {
+        "network": "8.8.8.0/24",
+        "autonomous_system_number": "AS15169",
+        "autonomous_system_organization": "GOOGLE"
+    }
+}
+*/
